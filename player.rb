79c9fffdf057f7862c186d2995e0caf37edd19c3
Javier Murillo <jhmmurillo@gmail.com>
2013-04-23 04:25:28 -0700

    level 2 complete

--- player.rb
+++ player.rb
@@ -1,5 +1,34 @@
 class Player
   def play_turn(warrior)
-    warrior.walk!(warrior.direction_of_stairs)
+    @health ||= warrior.health
+    @direction = warrior.direction_of_stairs
+    @warrior = warrior
+
+    if enemy?
+      warrior.attack!(@direction)
+    elsif taking_damage?
+      warrior.walk!(@direction)
+    elsif can_rest?
+      warrior.rest!
+    else
+      warrior.walk!(@direction)
+    end
+    @health = warrior.health
+  end
+
+  def enemy?
+    @warrior.feel(@direction).enemy?
+  end
+
+  def taking_damage?
+    @warrior.health < @health
+  end
+
+  def space?
+    @warrior.feel(@direction).empty?
+  end
+
+  def can_rest?
+    space? && @warrior.health < 20
   end
 end
